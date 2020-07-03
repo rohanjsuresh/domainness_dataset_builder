f29a3d6d86abd74dd751e6244047c0be5d989e05
rohansuresh <rohan.suresh12@gmail.com>
2020-07-03 15:02:42 -0500

    Updated URLs to work with dynamic URLs and not be hardcoded

--- domainness_dataset_builder/urls.py
+++ domainness_dataset_builder/urls.py
@@ -21,10 +21,9 @@ from django.contrib.auth import views as auth_views
 from users import views as user_views
 
 urlpatterns = [
-    path('home', include('demo_frontend.urls')),
-    path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path(r'', include('demo_frontend.urls')),
+    path(r'admin/', admin.site.urls),
+    path(r'register/', user_views.register, name='register'),
+    path(r'login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path(r'logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
