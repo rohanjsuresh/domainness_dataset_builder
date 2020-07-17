dfef00d59b4b20921a384edf0b500862f969b10d
rohansuresh <rohan.suresh12@gmail.com>
2020-07-17 15:55:35 -0500

    Added button to refresh page and provide new title without inserting into table

--- demo_frontend/admin.py
+++ demo_frontend/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from .models import Keywords
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
 from .models import UserExtension
+
+admin.site.register(Keywords)
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
