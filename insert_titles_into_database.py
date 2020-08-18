13ed5347c86d85ddc57797079d03fd12e177bbf6
rohansuresh <rohan.suresh12@gmail.com>
2020-08-18 12:04:07 -0500

    Modified models to show title subject (based of Arxiv classification)

--- insert_titles_into_database.py
+++ insert_titles_into_database.py
@@ -9,15 +9,22 @@ mydb = mysql.connector.connect(
 
 
 # Using readlines() 
-file1 = open('arxiv_titles.txt', 'r') 
-Lines = file1.readlines() 
+with open('cs_titles.json') as f:
+    Lines = [line.rstrip() for line in f]
   
 count = 0
 for line in Lines: 
     mycursor = mydb.cursor()
 
-    sql = "INSERT INTO demo_frontend_arxiv_titles_in_circulation (title, times_classified) VALUES (%s, %s)"
-    val = (line.strip(), "0")
+    jdata = json.loads(line)
+
+    sql = "INSERT INTO demo_frontend_arxiv_titles_in_circulation (title, subject, times_classified) VALUES (%s, %s, %s)"
+    val = (jdata["title"], jdata["subject"], "0")
     mycursor.execute(sql, val)
     mydb.commit()
     print(mycursor.rowcount, "record inserted.")
+
+
+
+with open('cs_titles.json') as f:
+    lines = [line.rstrip() for line in f]
